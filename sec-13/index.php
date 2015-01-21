5ee987fb30759ca1bb26bf4cc5658ee3b6eee2e7
Mehedi Hassan <MHDH331@gmail.com>
2015-01-22 00:35:08 +0600

    Update index.php

--- sec-13/index.php
+++ sec-13/index.php
@@ -7,7 +7,7 @@
 <body>
 	<center>
 		<h1>Welcome! 1337</h1><hr>
-		<h3>Hint: Post SQL Injection Error Based</h3><br>
+		<h3>Hint: Post SQL Injection - Error Based</h3><br>
 			<div align="center" style="margin:0 auto; background-color:#E8E8E8; border:1px solid #666; text-align:center; width:350px; height:130px; font-family:'Trebuchet MS', Arial, Helvetica, sans-serif;">
 				<div style="padding-top:10px; font-size:15px;">
 					<form action="" method="post">
@@ -31,14 +31,14 @@
 				if(isset($_POST['submit'])) {
 					$user = $_POST['user'];
 					$pass = $_POST['pass'];
-					$user ='"' .$user. '"';
-					$pass ='"' .$pass. '"'; 
+					$user = '"'.$user."'";
+					$pass = '"'.$pass."'";
 
-					$sql 	= "SELECT username, password, email FROM users WHERE username = ($user) AND password = ($pass) LIMIT 0,1";
+					$sql 	= "SELECT username, password, email FROM users WHERE username = $user AND password = $pass LIMIT 0,1";
 					$result = mysql_query($sql);
-					$row 	= mysql_fetch_array($result);
 
-					if($row) {
+					if(mysql_num_rows($result) > 0) {
+						$row = mysql_fetch_array($result);
 					  	echo '<br>Your E-Mail : ', $row['email'], '<br><br> <img src="../images/ok.png" >';
 				  	} else {
 					  	echo print_r(mysql_error(), true), '<br> <img src="../images/error.png">';
